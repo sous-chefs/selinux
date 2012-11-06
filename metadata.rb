06b48f42711a4427dd091a4eac898c9e7eafb4cc
Abel <alopgeek@gmail.com>
2012-11-05 17:15:11 -0800

    silly me, you can't have hyphens

--- metadata.rb
+++ metadata.rb
@@ -4,3 +4,8 @@ license          "Apache"
 description      "Installs/Configures selinux"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.5.3"
+
+attribute "selinux/state"
+  :display_name => "SELinux state"
+  :description => "Choose from Enforcing, Permissive, Disabled"
+  :default => "permissive"
