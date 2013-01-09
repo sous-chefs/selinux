9350fb2e1a095fcc582b607aadcfef3614926a44
jtimberman <joshua@opscode.com>
2013-01-08 17:04:12 -0700

    release v0.5.6

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email "someara@opscode.com"
 license          "Apache"
 description      "Installs/Configures selinux"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.5.4"
+version          "0.5.6"
