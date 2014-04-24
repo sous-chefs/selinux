348aef007453235f9de64638086a9c7c50478021
Christo De Lange <cdelange@amplify.com>
2014-04-23 20:36:58 -0700

    [COOK-4562] - Basic support for Ubuntu/Debian
    Signed-off-by: Sean OMeara <someara@opscode.com>

--- recipes/_common.rb
+++ recipes/_common.rb
@@ -1,4 +1,13 @@
-package 'libselinux-utils'
+
+case node[:platform_family]
+when %r(debian|ubuntu)
+  package 'selinux-utils'
+when 'rhel'
+  package 'libselinux-utils'
+else
+    # type code here
+    raise "#{node[:platform_family]} not supported!"
+end
 
 directory '/etc/selinux' do
   owner 'root'
