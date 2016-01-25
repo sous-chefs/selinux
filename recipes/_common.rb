d742ef4c0b89f03b72e0ef19197bc5f064c613b0
John Bartko <john.bartko@drillinginfo.com>
2016-01-25 17:43:52 -0600

    handle required packages as attribute
    This allows greater flexibility in use of the cookbook

--- recipes/_common.rb
+++ recipes/_common.rb
@@ -1,30 +1,10 @@
-
 case node['platform_family']
-when %r(debian|ubuntu)
+when 'debian'
   include_recipe 'apt'
-  if node['platform_version'].to_f <= 12
-    %w(selinux-utils selinux-policy-default selinux-basics auditd).each { |pkg|
-      package pkg
-    }
-  elsif
-    %w(selinux selinux-utils selinux-policy-ubuntu selinux-basics auditd).each { |pkg|
-      package pkg if node['selinux']['state'] == 'enabled'
-    }
-  end
-  include_recipe 'selinux::debian'
 when %r(fedora|rhel)
-  if node['platform_version'].to_f >= 7.0
-    include_recipe 'yum::dnf_yum_compat'
-  end
-  package 'libselinux-utils'
-else
-    # implement support for your platform here!
-    raise "#{node['platform_family']} not supported!"
+  include_recipe 'yum::dnf_yum_compat'
 end
 
-directory '/etc/selinux' do
-  owner 'root'
-  group 'root'
-  mode '0755'
-  action if node['selinux']['state'] == 'disabled' ? :nothing : :create
-end
+package node['selinux']['packages']
+
+include_recipe 'selinux::debian' if platform_family?('debian')
