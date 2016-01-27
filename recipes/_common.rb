da568a8cba7a37991983197ad41150d7c1087dbf
John Bartko <john.bartko@drillinginfo.com>
2016-01-26 23:18:13 -0600

    iterate over packages
    preserves Chef 11 compatibility

--- recipes/_common.rb
+++ recipes/_common.rb
@@ -5,6 +5,8 @@ when %r(fedora|rhel)
   include_recipe 'yum::dnf_yum_compat'
 end
 
-package node['selinux']['packages']
+node['selinux']['packages'].each { |pkg|
+  package pkg
+}
 
 include_recipe 'selinux::debian' if platform_family?('debian')
