108e0707fa11aa374ae6d33ffebab5a13a89a46a
Jennifer Davis <iennae@gmail.com>
2017-02-24 23:41:00 -0800

    Fixing lint errors Signed-off-by: Jennifer Davis <iennae@gmail.com>

--- libraries/matchers.rb
+++ libraries/matchers.rb
@@ -12,7 +12,7 @@ if defined?(ChefSpec)
   def permissive_selinux_state(resource_name)
     ChefSpec::Matchers::ResourceMatcher.new(:selinux_state, :permissive, resource_name)
   end
- 
+
   ChefSpec.define_matcher :selinux_install
 
   def install_selinux_install(resource_name)
