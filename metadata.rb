c9b9b1106f3e522db353a895281d816058e7db3e
Tim Smith <tsmith@chef.io>
2017-04-08 00:09:44 -0700

    Use the standard apache license string
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'selinux'
 maintainer       'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
-license          'Apache'
+license          'Apache-2.0'
 description      'Manages SELinux policy state'
 version          '1.0.3'
 
@@ -11,4 +11,4 @@ end
 
 source_url 'https://github.com/chef-cookbooks/selinux'
 issues_url 'https://github.com/chef-cookbooks/selinux/issues'
-chef_version '>= 12.7'
+chef_version '>= 12.7' if respond_to?(:chef_version)
