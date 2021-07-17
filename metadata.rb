92fd3f6ab1d4ceb2f8421023b645c52a7f881608
Robert Detjens <detjensrobert@osuosl.org>
2021-07-16 17:26:45 -0700

    Update README
    Signed-off-by: Robert Detjens <detjensrobert@osuosl.org>

--- metadata.rb
+++ metadata.rb
@@ -1,14 +1,13 @@
 name             'selinux'
-maintainer       'Chef Software, Inc.'
-maintainer_email 'cookbooks@chef.io'
+maintainer       'Sous Chefs'
+maintainer_email 'help@sous-chefs.org'
 license          'Apache-2.0'
 description      'Manages SELinux policy state and rules.'
 version          '3.1.1'
+source_url       'https://github.com/sous-chefs/selinux'
+issues_url       'https://github.com/sous-chefs/selinux/issues'
+chef_version     '>= 15.3'
 
 %w(redhat centos scientific oracle amazon fedora).each do |os|
   supports os
 end
-
-source_url 'https://github.com/chef-cookbooks/selinux'
-issues_url 'https://github.com/chef-cookbooks/selinux/issues'
-chef_version '>= 13.0'
