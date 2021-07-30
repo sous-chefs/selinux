93ebcecd6f1637118e7d632627e7b47de7b69d45
Benjamin M. Hughes <bmhughes@bmhughes.co.uk>
2021-07-30 19:46:48 +0100

    Debian requires a module loading to allow module loading
    Yes, you read that correctly ;)

--- test/integration/enforcing/state_spec.rb
+++ test/integration/enforcing/state_spec.rb
@@ -12,11 +12,9 @@ describe selinux do
   it { should_not be_disabled }
   it { should be_enforcing }
   it { should_not be_permissive }
-  its('policy') { should eq 'targeted' }
-end
-
-describe selinux.modules.where('test') do
-  it { should exist }
-  it { should be_installed }
-  it { should be_enabled }
+  if os.family.eql?('debian')
+    its('policy') { should eq 'default' }
+  else
+    its('policy') { should eq 'targeted' }
+  end
 end
