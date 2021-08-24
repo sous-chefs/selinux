1deb9de7ea564117882cad996353ff8966bd6102
Robert Detjens <detjensrobert@osuosl.org>
2021-08-24 16:17:51 -0700

    Fix test idempotency
    Signed-off-by: Robert Detjens <detjensrobert@osuosl.org>

--- test/integration/enforcing/controls/module_control.rb
+++ test/integration/enforcing/controls/module_control.rb
@@ -2,9 +2,9 @@ control 'module' do
   title 'Verify that SELinux modules are installed correctly'
 
   describe selinux.modules.where(name: 'test') do
-    it { should exist }
-    it { should be_installed }
-    it { should be_enabled }
+    it { should_not exist }
+    it { should_not be_installed }
+    it { should_not be_enabled }
   end
 
   if os.family.eql?('debian')
--- test/integration/permissive/controls/module_control.rb
+++ test/integration/permissive/controls/module_control.rb
@@ -2,8 +2,8 @@ control 'module' do
   title 'Verify that SELinux modules are installed correctly'
 
   describe selinux.modules.where(name: 'test') do
-    it { should_not exist }
-    it { should_not be_installed }
-    it { should_not be_enabled }
+    it { should exist }
+    it { should be_installed }
+    it { should be_enabled }
   end
 end
